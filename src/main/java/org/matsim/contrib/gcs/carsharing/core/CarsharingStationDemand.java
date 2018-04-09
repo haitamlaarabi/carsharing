da33537bcba1ddbdd5a1bd1edc2e6a78f7479d3a
haitam <htmlrb@gmail.com>
2018-04-09 12:41:01 +0200

    abstract relocation moved

---
+++ src/main/java/org/matsim/contrib/gcs/carsharing/core/CarsharingStationDemand.java
@@ -0,0 +1,87 @@
+package org.matsim.contrib.gcs.carsharing.core;
+
+import org.apache.log4j.Logger;
+import org.matsim.contrib.gcs.carsharing.core.CarsharingBookingStation.BookingRecordWrapper;
+
+public class CarsharingStationDemand {
+
+	private static Logger logger = Logger.getLogger(CarsharingStationDemand.class);
+	
+	CarsharingBookingStation b;
+
+	public CarsharingStationDemand(CarsharingBookingStation b) {
+		b = new CarsharingBookingStation(b);
+	}
+	
+	public CarsharingStationDemand(CarsharingStationMobsim s) {
+		b = new CarsharingBookingStation(s);
+	}	
+	
+	public CarsharingStationDemand(CarsharingStation s) {
+		b = new CarsharingBookingStation(s);
+	}	
+	
+	
+	public boolean push(CarsharingBookingRecord dm) {
+		BookingRecordWrapper w = b.new BookingRecordWrapper(dm);
+		if(w.isDemand) {
+			b.car_availability_tracker -= dm.getNbrOfVeh(); 
+		} else {
+			b.parking_availability_tracker -= dm.getNbrOfVeh();
+		}
+		if(b.car_availability_tracker < 0 || b.parking_availability_tracker > b.station.parking().getCapacity()) {
+			logger.warn("Availability in station "+ b.station + " is " + b.car_availability_tracker);
+		}
+		w.car_availability_flag = b.car_availability_tracker;
+		w.parking_availability_flag = b.parking_availability_tracker;
+		b.activity.add(w);
+		b.booking_wrapper.put(dm, w);
+		return true;
+	}
+	
+	public int vehicleMinAvailability(int V0, double lb, double up) {
+		int min_cav = V0;
+		int av = min_cav;
+		for(BookingRecordWrapper w : b.activity.subList(b.new BookingRecordWrapper(lb), b.new BookingRecordWrapper(up))) {
+			if(w.isDemand) 
+				av -= w.record.getNbrOfVeh();
+			else if(!w.isDemand) 
+				av += w.record.getNbrOfVeh();
+			if(av < min_cav) 
+				min_cav = av;
+		}
+		return min_cav;
+	}
+	
+	public int parkingMinAvailability(int V0, double lb, double up) {
+		int min_pav = b.station.parking().getCapacity() - V0;
+		int pav = min_pav;
+		for(BookingRecordWrapper w : b.activity.subList(b.new BookingRecordWrapper(lb), b.new BookingRecordWrapper(up))) {
+			if(w.isDemand) 
+				pav += w.record.getNbrOfVeh();
+			else if(!w.isDemand) 
+				pav -= w.record.getNbrOfVeh();
+			if(pav < min_pav) 
+				min_pav = pav;
+		}
+		return min_pav;
+	}
+	
+	public CarsharingStationMobsim getStation() {
+		return this.b.getStation();
+	}
+	
+	public CarsharingBookingStation getBooking() {
+		return this.b;
+	}
+	
+
+	double coef;
+	public double getCoef() {
+		return coef;
+	}
+	public void setCoeff(double coefficient) {
+		coef = coefficient;
+	}	
+	
+}
---
+++ src/main/java/org/matsim/contrib/gcs/config/CarsharingRelocationParams.java
@@ -0,0 +1,202 @@
+package org.matsim.contrib.gcs.config;
+
+import org.matsim.core.api.internal.MatsimParameters;
+import org.matsim.core.config.ReflectiveConfigGroup;
+
+public class CarsharingRelocationParams extends ReflectiveConfigGroup implements MatsimParameters {
+	
+	final static String SET_TYPE = "carsharingRelocationParams";
+
+
+	private int activate_from_iter = 0;
+	private int deactivate_after_iter = 0;
+	private int binstats_lbound = 1800;
+	private int binstats_ubound = 1800;
+	//private int statsbin_step = 0;
+	private int staff_lbound = 1;
+	private int staff_ubound = 1;
+	private int maxtrain =1;
+
+
+
+	private String perfomance_file = null;
+	private String task_output_file = null;
+	private String trace_output_file = null;
+	private String estimated_demand_file = null;
+
+
+
+	CarsharingRelocationParams() {
+		super( SET_TYPE );
+	}
+	
+	/**
+	 * @return the maxtrain
+	 */
+	public int getMaxtrain() {
+		return maxtrain;
+	}
+
+
+	/**
+	 * @param maxtrain the maxtrain to set
+	 */
+	public void setMaxtrain(int maxtrain) {
+		this.maxtrain = maxtrain;
+	}
+	
+	/**
+	 * @return the task_file
+	 */
+	public String getTask_output_file() {
+		return task_output_file;
+	}
+
+	/**
+	 * @param task_file the task_file to set
+	 */
+	public void setTask_output_file(String task_file) {
+		this.task_output_file = task_file;
+	}
+
+	/**
+	 * @return the trace_file
+	 */
+	public String getTrace_output_file() {
+		return trace_output_file;
+	}
+
+	/**
+	 * @param trace_file the trace_file to set
+	 */
+	public void setTrace_output_file(String trace_file) {
+		this.trace_output_file = trace_file;
+	}
+	
+	/**
+	 * @return the activate_from_iter
+	 */
+	public int getActivate_from_iter() {
+		return activate_from_iter;
+	}
+
+
+	/**
+	 * @param activate_from_iter the activate_from_iter to set
+	 */
+	public void setActivate_from_iter(int activate_from_iter) {
+		this.activate_from_iter = activate_from_iter;
+	}
+
+
+	/**
+	 * @return the deactivate_after_iter
+	 */
+	public int getDeactivate_after_iter() {
+		return deactivate_after_iter;
+	}
+
+
+	/**
+	 * @param deactivate_after_iter the deactivate_after_iter to set
+	 */
+	public void setDeactivate_after_iter(int deactivate_after_iter) {
+		this.deactivate_after_iter = deactivate_after_iter;
+	}
+
+
+	/**
+	 * @return the lower_bound_bin
+	 */
+	public int getBinstats_lbound() {
+		return binstats_lbound;
+	}
+
+
+	/**
+	 * @param lower_bound_bin the lower_bound_bin to set
+	 */
+	public void setStatsbin_lbound(int lower_bound_bin) {
+		this.binstats_lbound = lower_bound_bin;
+	}
+
+
+	/**
+	 * @return the upper_bound_bin
+	 */
+	public int getBinstats_ubound() {
+		return binstats_ubound;
+	}
+
+
+	/**
+	 * @param upper_bound_bin the upper_bound_bin to set
+	 */
+	public void setStatsbin_ubound(int upper_bound_bin) {
+		this.binstats_ubound = upper_bound_bin;
+	}
+
+
+	/**
+	 * @return the lower_bound_staff
+	 */
+	public int getStaff_lbound() {
+		return staff_lbound;
+	}
+
+
+	/**
+	 * @param lower_bound_staff the lower_bound_staff to set
+	 */
+	public void setStaff_lbound(int lower_bound_staff) {
+		this.staff_lbound = lower_bound_staff;
+	}
+
+
+	/**
+	 * @return the upper_bound_staff
+	 */
+	public int getStaff_ubound() {
+		return staff_ubound;
+	}
+
+
+	/**
+	 * @param upper_bound_staff the upper_bound_staff to set
+	 */
+	public void setStaff_ubound(int upper_bound_staff) {
+		this.staff_ubound = upper_bound_staff;
+	}
+
+
+	/**
+	 * @return the perfomance_file
+	 */
+	public String getPerfomance_file() {
+		return perfomance_file;
+	}
+
+
+	/**
+	 * @param perfomance_file the perfomance_file to set
+	 */
+	public void setPerfomance_file(String perfomance_file) {
+		this.perfomance_file = perfomance_file;
+	}
+
+
+	/**
+	 * @return the estimated_demand_file
+	 */
+	public String getEstimated_demand_file() {
+		return estimated_demand_file;
+	}
+
+
+	/**
+	 * @param estimated_demand_file the estimated_demand_file to set
+	 */
+	public void setEstimated_demand_file(String estimated_demand_file) {
+		this.estimated_demand_file = estimated_demand_file;
+	}
+}
